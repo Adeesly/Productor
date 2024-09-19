ac2bf38c6cf3d86cb1139c609cce9d45b03b0f74
Adeesly <past.21.01@gmail.com>
2024-09-18 22:27:31 -0500

    cambios

---
+++ src/main/java/com/prestamo/controller/CatalogoController.java
@@ -0,0 +1,31 @@
+package com.prestamo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prestamo.entity.Catalogo;
+import com.prestamo.service.CatalogoService;
+
+@RestController
+@RequestMapping("/url/catalogo")
+public class CatalogoController {
+
+    @Autowired
+    private CatalogoService catalogoService;
+
+    @GetMapping("/lista")
+    public List<Catalogo> getAllCatalogo() {
+        return catalogoService.findAll();
+    }
+
+    @PostMapping("/inserta")
+    public Catalogo insertCatalogo(@RequestBody Catalogo catalogo) {
+        return catalogoService.insertaCatalogo(catalogo);
+    }
+}
